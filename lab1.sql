6f7645f16acbf5bca78a8484f707217901950399
sraziyev <s_raziyev@kbtu.kz>
2024-09-18 18:15:05 +0500

    Add files via upload

---
+++ lab1.sql
@@ -0,0 +1,18 @@
+CREATE DATABASE lab1;
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    firstname VARCHAR(50),
+    lastname VARCHAR(50)
+);
+ALTER TABLE users ADD COLUMN isadmin INT(1);
+ALTER TABLE users ADD COLUMN is_admin BOOLEAN DEFAULT FALSE;
+UPDATE users
+SET is_admin = (isadmin = 1);
+ALTER TABLE users DROP COLUMN isadmin;
+CREATE TABLE tasks (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50),
+    user_id INT
+);
+DROP TABLE tasks;
+DROP DATABASE lab1;
